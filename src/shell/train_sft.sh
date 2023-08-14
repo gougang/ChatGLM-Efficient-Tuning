0959c7c3bee0044090e05cc51555ad78c962c3c1
苟刚 <gang.gou@ly.com>
2023-08-14 15:56:35 +0800

    update param

--- src/shell/train_sft.sh
+++ src/shell/train_sft.sh
@@ -9,9 +9,9 @@ CUDA_VISIBLE_DEVICES=0 python ../train_bash.py \
     --finetuning_type lora \
     --output_dir covid/sft \
     --overwrite_cache \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 40 \
+    --per_device_eval_batch_size 40 \
+    --gradient_accumulation_steps 16 \
     --max_source_length 1024 \
     --max_target_length 128 \
     --lr_scheduler_type cosine \
